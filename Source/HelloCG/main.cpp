cf9c8fa6320f602795f934063711457f4182518b
toulzx <toulzx2021@gmail.com>
2021-11-08 21:09:22 +0800

    [Week2-2&3-1]使用着色器绘制一个三角形

--- Source/HelloCG/main.cpp
+++ Source/HelloCG/main.cpp
@@ -6,9 +6,27 @@
 #include <GLFW/glfw3.h>
 
 
-
+// 窗口大小
 const GLint WIDTH = 800, HEIGHT = 600;
 
+
+// 顶点着色器
+const GLchar* vertexShaderCode = 
+	"#version 330 core\n"
+	"layout(location = 0) in vec3 position;\n"
+	"void main()\n"
+	"{\n"
+	"gl_Position = vec4(position, 1.0f);\n"
+	"}";
+
+// 片元着色器 
+const GLchar* fragmentShaderCode = 
+	"#version 330 core\n"
+	"out vec4 color;\n"
+	"void main()\n"
+	"{\n"
+	"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}";
 
 
 int main()
@@ -49,6 +67,75 @@ int main()
 	}
 
 
+	// 状态信息
+	GLint success;
+	// 日志
+	GLchar infoLog[512];
+
+	// 导入并编译顶点着色器
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderCode, NULL);
+	glCompileShader(vertexShader);
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) 
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// 导入并编译片元着色器
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderCode, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) 
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// 创建并链接形成可在 GPU 上直接执行的文件
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	// 顶点位置集
+	GLfloat vertices[] =
+	{	// position
+		-0.5f, -0.5f,  0.0f,
+		 0.5f, -0.5f,  0.0f,
+		 0.0f,  0.5f,  0.0f
+	};
+
+	// 创建顶点正面对象（VAO）、顶点缓存对象（VBO）
+	GLuint VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	// 绑定 VAO、VBO
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	// 数据传入显卡
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// 设置 VAO
+	// 对应此句："layout(location = 0) in vec3 position;" 和上述 vertices[]
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	// 解绑定 VAO、VBO
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+
 	// 逐帧画图
 	while (!glfwWindowShouldClose(window))
 	{
@@ -56,11 +143,25 @@ int main()
 		glfwPollEvents();
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// 使用着色器绘制一个三角形
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
 		glfwSwapBuffers(window);
 	}
 
 	
 	glfwTerminate();
+
+
+	// 释放
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
 
 	return 0;
 

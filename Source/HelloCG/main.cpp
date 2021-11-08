dc70d77b6df79eb13c22d53ca1cc291f37653c03
toulzx <toulzx2021@gmail.com>
2021-11-08 18:42:52 +0800

    [Week2-1]梳理、重写 Demo

--- Source/HelloCG/main.cpp
+++ Source/HelloCG/main.cpp
@@ -4,40 +4,64 @@
 #include <GL/glew.h>
 //GLFW
 #include <GLFW/glfw3.h>
+
+
+
 const GLint WIDTH = 800, HEIGHT = 600;
+
+
+
 int main()
 {
+
 	glfwInit();
+
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // must for Mac
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Learn OpenGL", nullptr, nullptr);
-	// next two lines are for mac retina display
-	int screenWidth, screenHeight;
-	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+	
 	if (nullptr == window)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
+
+	// 获取显存空间下实际的窗口大小
+	int screenWidth, screenHeight;
+	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+
+	// 设置焦点为当前窗口
 	glfwMakeContextCurrent(window);
+	
+
 	glewExperimental = GL_TRUE;
+
 	if (GLEW_OK != glewInit())
 	{
 		std::cout << "Failed to initialise GLEW" << std::endl;
+		glfwTerminate();
 		return -1;
 	}
-	glViewport(0, 0, screenWidth, screenHeight);
+
+
+	// 逐帧画图
 	while (!glfwWindowShouldClose(window))
 	{
+		glViewport(0, 0, screenWidth, screenHeight);
 		glfwPollEvents();
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glfwSwapBuffers(window);
 	}
+
+	
 	glfwTerminate();
+
 	return 0;
+
 }

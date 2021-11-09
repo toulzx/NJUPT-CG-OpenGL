6ec1a01707344ef61b6bd6de9fe1f65c45a2b8b1
toulzx <toulzx2021@gmail.com>
2021-11-09 22:01:09 +0800

    [Week5-1]清理代码，准备学习纹理部分

--- Source/HelloCG/main.cpp
+++ Source/HelloCG/main.cpp
@@ -49,12 +49,10 @@ int main()
 	}
 
 
-	// 将两着色器文件传入 Shader 头文件
 	Shader ourShader = Shader("res/shaders/core.vs", "res/shaders/core.fs");
 
 
-	// Week5-1 使用点和连接信息来实现原先的矩形（正方形）
-	// 定义点和颜色
+	// 使用点和连接信息构建
 	GLfloat vertices[] =
 	{	// position							// color
 		 0.5f,  0.5f,  0.0f,				 1.0f,  0.0f,  0.0f,		// 右上
@@ -62,39 +60,31 @@ int main()
 		-0.5f, -0.5f,  0.0f,				 1.0f,  0.0f,  0.0f,		// 左下
 		-0.5f,  0.5f,  0.0f,				 1.0f,  0.0f,  0.0f,		// 左上
 	};
-	// 定义连接信息
 	unsigned int indices[] =
 	{
 		0, 1, 3,				// 第一个三角形
 		1, 2, 3					// 第二个三角形
 	};
 
-	// 创建顶点正面对象（VAO）、顶点缓存对象（VBO）
-	GLuint VAO, VBO;
+
+	// VAO、VBO、EBO 的创建、绑定、设置、解绑
+	GLuint VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	// 绑定 VAO、VBO
+	glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// 数据传入显卡
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	// 设置 VAO
-	// 对应 core.vs 中此句："layout(location = 0) in vec3 position;" 和本文上述 vertices[] "position" 部分
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-	// 对应 core.vs 中此句："layout(location = 1) in vec3 ourColor;" 和本文上述 vertices[] "color"部分
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
 
-	// 创建元素缓冲对象（EBO）
-	GLuint EBO;
-	// 绑定 EBO
-	glGenBuffers(1, &EBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	// 数据传入显卡
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// 解绑定 EBO、VBO、VAO
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);

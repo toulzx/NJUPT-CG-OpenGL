75e411d6e863007e5a90a2cb76410e4045b97491
toulzx <toulzx2021@gmail.com>
2021-11-09 21:53:17 +0800

    [Week5-1]改用点和连接信息绘制正方形

--- Source/HelloCG/main.cpp
+++ Source/HelloCG/main.cpp
@@ -53,32 +53,31 @@ int main()
 	Shader ourShader = Shader("res/shaders/core.vs", "res/shaders/core.fs");
 
 
-	// 顶点位置集 rectangle
+	// Week5-1 使用点和连接信息来实现原先的矩形（正方形）
+	// 定义点和颜色
 	GLfloat vertices[] =
-	{	// position						// color
-		// first triangle
-		 0.5f,  0.5f,  0.0f,			 1.0f,  0.0f,  0.0f,
-		 0.5f, -0.5f,  0.0f,			 1.0f,  0.0f,  0.0f,
-		-0.5f,  0.5f,  0.0f,			 1.0f,  0.0f,  0.0f,
-
-		// second triangle
-		 0.5f, -0.5f,  0.0f,			 0.0f,  1.0f,  0.0f,
-		-0.5f, -0.5f,  0.0f,			 0.0f,  1.0f,  0.0f,
-		-0.5f,  0.5f,  0.0f,			 0.0f,  1.0f,  0.0f
+	{	// position							// color
+		 0.5f,  0.5f,  0.0f,				 1.0f,  0.0f,  0.0f,		// 右上
+		 0.5f, -0.5f,  0.0f,				 1.0f,  0.0f,  0.0f,		// 右下
+		-0.5f, -0.5f,  0.0f,				 1.0f,  0.0f,  0.0f,		// 左下
+		-0.5f,  0.5f,  0.0f,				 1.0f,  0.0f,  0.0f,		// 左上
+	};
+	// 定义连接信息
+	unsigned int indices[] =
+	{
+		0, 1, 3,				// 第一个三角形
+		1, 2, 3					// 第二个三角形
 	};
 
 	// 创建顶点正面对象（VAO）、顶点缓存对象（VBO）
 	GLuint VAO, VBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-
 	// 绑定 VAO、VBO
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
 	// 数据传入显卡
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
 	// 设置 VAO
 	// 对应 core.vs 中此句："layout(location = 0) in vec3 position;" 和本文上述 vertices[] "position" 部分
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
@@ -87,7 +86,16 @@ int main()
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
 
-	// 解绑定 VAO、VBO
+	// 创建元素缓冲对象（EBO）
+	GLuint EBO;
+	// 绑定 EBO
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	// 数据传入显卡
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// 解绑定 EBO、VBO、VAO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
@@ -100,12 +108,12 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// 使用着色器绘制一个三角形
 		ourShader.Use();
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 		glBindVertexArray(0);
-
 		glfwSwapBuffers(window);
 	}
 
@@ -116,7 +124,7 @@ int main()
 	// 释放
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-
+	glDeleteBuffers(1, &EBO);
 
 	return 0;
 
